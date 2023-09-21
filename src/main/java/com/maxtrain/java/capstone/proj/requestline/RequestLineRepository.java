467221c9488d60ec14f6d285a51443a835a67cf4
DrewBoschert <drew.r.boschert@gmail.com>
2023-09-21 17:52:11 -0400

    Added Recalculate

--- src/main/java/com/maxtrain/java/capstone/proj/requestline/RequestLineRepository.java
+++ src/main/java/com/maxtrain/java/capstone/proj/requestline/RequestLineRepository.java
@@ -1,17 +1,10 @@
 package com.maxtrain.java.capstone.proj.requestline;
-
-import java.util.List;
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RequestLineRepository extends CrudRepository<RequestLine, Integer>{
-	/*
-	@Query("SELECT SUM(rl.Quantity * p.Price) AS LineTotal\r\n"
-			+ "FROM RequestLines AS rl\r\n"
-			+ "JOIN Products AS p ON rl.ProductId = p.Id\r\n"
-			+ "WHERE rl.userId = userId;\r\n"
-			+ "")
-	List<RequestLine> RecalculateTotal(int requestId);
-	*/
+	
+	@Query("SELECT SUM(rl.quantity * p.price) FROM RequestLine rl JOIN rl.product p WHERE rl.request.id = :requestId")
+	double calculateTotal(int requestId);
+	
 }
